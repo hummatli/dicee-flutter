72672607667c81490e62635496712f3d13f858fa
Sattar Hummatli <shummatli@Sattars-MacBook-Pro.local>
2020-02-18 16:51:43 +0400

    Added FlatButton

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,27 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left button got pressed');
+              },
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right button');
+              },
+              child: Image.asset("images/dice2.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
